6ba6d35cb20199dbd946b2166b4fe19cca2e601f
Jaiden Douglas <49811314+ja1den@users.noreply.github.com>
2021-08-22 21:26:09 +0930

    build(client): add bootstrap

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,49 +1,22 @@
 // Import
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-import axios from 'axios';
+import { Container } from 'react-bootstrap';
 
 import './styles/global.scss';
 
-import colors from '@lib/colors';
-
 // Root Component
 const Root: React.FC = () => {
-	const [color, setColor] = useState<string | null>(null);
-
-	// Request a Color
-	const requestColor = () => {
-		axios.get('/api/color').then(res => setColor(res.data))
-			.catch(console.error);
-	};
-
-	// Emit Request
-	useEffect(() => void requestColor(), []);
-
-	// Bind 'onClick'
-	useEffect(() => {
-		document.body.onclick = requestColor;
-
-		return () => {
-			document.body.onclick = null;
-		};
-	}, [color]);
-
-	// List Elements
-	const elements: JSX.Element[] = [];
-
-	for (let i = 0; i < colors.length; i++) {
-		elements.push(
-			<li key={i} style={{
-				backgroundColor:
-					colors[i] === color ? colors[i] : '#EEE'
-			}} />
-		);
-	}
-
 	// Render HTML
-	return <ul className='colors' children={elements} />;
+	return (
+		<Container>
+			<header>
+				<h1>PointBase</h1>
+				<p>Point tracking system for Sports Day.</p>
+			</header>
+		</Container>
+	);
 }
 
 // Render to DOM
