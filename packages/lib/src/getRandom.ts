6a5f41ce60d308e5059768e56e92cd473e762362
Jaiden Douglas <49811314+ja1den@users.noreply.github.com>
2021-08-22 21:26:54 +0930

    chore: remove lib

--- packages/lib/src/getRandom.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Generate a random number between min and max.
- * @param min The minimum number, inclusive.
- * @param max The maximum number, exclusive.
- * @returns The random number.
- */
-export default function getRandom(min: number, max: number) {
-	return Math.floor(Math.random() * (max - min)) + min;
-}
